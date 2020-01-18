65f39852ef4170e526fdcdc115ac700dc6c505d8
Hubert Kazmierczak <hkaz@mbp-hkaz.local>
2020-01-18 12:30:53 +0100

    investigate travis test error

--- server/tests/connectTestDB.ts
+++ server/tests/connectTestDB.ts
@@ -3,9 +3,11 @@ import { connectDB } from "../src/connectDB";
 
 export const connectTestDB = async (): Promise<Connection> =>
   connectDB("development", {
-    dropSchema: true,
     host: "localhost",
     username: "realworld",
     password: "realworld",
     database: "test",
+    synchronize: true,
+    retryAttempts: 2,
+    retryDelay: 1000,
   });
