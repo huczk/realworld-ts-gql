959060d11a49002f7d0e9ad27ce80b7d4b32a44e
Hubert Kazmierczak <hkaz@mbp-hkaz.local>
2020-01-18 12:41:04 +0100

    investigate travis test error

--- server/src/connectDB.ts
+++ server/src/connectDB.ts
@@ -1,14 +1,7 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export const connectDB = async (
-  type?: "development" | "production",
-  override: any = {},
-): Promise<Connection> =>
+export const connectDB = async (): Promise<Connection> =>
   createConnection({
-    ...(await getConnectionOptions(
-      type || process.env.NODE_ENV || "development",
-    )),
-    ...override,
-
+    ...(await getConnectionOptions(process.env.NODE_ENV || "development")),
     name: "default",
   });
--- server/tests/connectTestDB.ts
+++ server/tests/connectTestDB.ts
@@ -1,13 +1,7 @@
-import { Connection } from "typeorm";
-import { connectDB } from "../src/connectDB";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export const connectTestDB = async (): Promise<Connection> =>
-  connectDB("development", {
-    host: "localhost",
-    username: "realworld",
-    password: "realworld",
-    database: "test",
-    synchronize: true,
-    retryAttempts: 2,
-    retryDelay: 1000,
+  createConnection({
+    ...(await getConnectionOptions()),
+    name: "default",
   });
